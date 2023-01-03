358ef29af9cbe1d5f9a90e08c501ced30e410315
novelkhan <novel4004@gmail.com>
2023-01-04 02:33:56 +0600

    Add project files.

---
+++ FileUploader/Interfaces/ICommon.cs
@@ -0,0 +1,14 @@
+﻿using FileUploader.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace FileUploader.Interfaces
+{
+    public interface ICommon
+    {
+        string UploadedFile(IFormFile ProfilePicture);
+        Tuple<byte[], string> GetDownloadDetails(long id);
+        Task<AttachmentFile> AddAttachmentFile(IFormFile _IFormFile);
+    }
+}
